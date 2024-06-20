b40c3b18f08ec4ac3c6ad13effc7d891f65f8079
thomasmfx <thomasmoisesf@gmail.com>
2024-06-20 16:58:11 -0300

    feat: Add functions do add/remove button's background color
    If current tab is 'Home', its button bg will be brown

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import './style.css';
 import showHomeTab from './home.js';
 
-window.onload = showHomeTab();
-
 const homeBtn = document.querySelector('#home');
+const menuBtn = document.querySelector('#menu');
+const contactBtn = document.querySelector('#contact');
+const aboutBtn = document.querySelector('#about');
+
+window.onload = paintBtn(homeBtn), showHomeTab();
+
 homeBtn.addEventListener('click', () =>{
+    removeBtnsPaint();
+    paintBtn(homeBtn);
     showHomeTab();
 });
 
 
+
+function paintBtn(btn) {
+    btn.className = 'current-tab';
+};
+
+function removeBtnsPaint(){
+    const btns = document.querySelectorAll('button');
+    btns.forEach(btn => {
+        btn.className = '';
+    });
+};
