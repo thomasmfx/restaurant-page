f28424ff8f611161456cd85d89727b5cd8509aa8
thomasmfx <thomasmoisesf@gmail.com>
2024-06-20 00:36:04 -0300

    build: Add source map to track errors

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
