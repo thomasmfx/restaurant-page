64cbfaa4293d2ab4299f467bdc121226198cc581
thomasmfx <thomasmoisesf@gmail.com>
2024-06-21 18:09:58 -0300

    feat: Add Contact's content and tab changing behavior

---
+++ src/contact.js
@@ -0,0 +1,52 @@
+import {
+    createElement,
+    createElementWithId,
+    createElementWithClass,
+    createImgWithSrc,
+    removeCurrentTab
+} from './domManipulation.js';
+
+const container = document.querySelector('#container');
+
+export default function showContactTab() {
+    const content = createElementWithClass('div', 'content');
+    const meansOfContat = createElementWithId('div', 'means-of-contact')
+    if(content.id !== 'contact-tab') {
+        removeCurrentTab();
+        content.appendChild(createElementWithId('h1', 'heading', 'Soul Coffee'));
+        content.id = 'contact-tab';
+
+        meansOfContat.append(
+            meanTemplate('../assets/contact/map-pin.svg', 'Address', '771 Flavor Street, Culinary City'),
+            meanTemplateTwo('Phone', '../assets/contact/phone.svg', '(515) 263-4847'),
+            meanTemplate('../assets/contact/instagram.svg', 'Instagram', '@soulcoffee'),
+            meanTemplateTwo('E-mail', '../assets/contact/mail.svg', 'soul.coffee@gmail.com')
+        );
+        content.appendChild(meansOfContat);
+        container.appendChild(content);
+    };
+};
+
+// Icon b4 heading
+function meanTemplate(iconSrc, heading, text) {
+    const mean = createElementWithClass('div', 'mean')
+    mean.append(
+        createImgWithSrc(`${iconSrc}`),
+        createElement('h2', `${heading}`),
+        createElement('p', `${text}`)
+    );
+
+    return mean;
+};
+
+// Heading b4 icon
+function meanTemplateTwo(heading, iconSrc, text) {
+    const mean = createElementWithClass('div', 'mean')
+    mean.append(
+        createElement('h2', `${heading}`),
+        createImgWithSrc(`${iconSrc}`),
+        createElement('p', `${text}`)
+    );
+
+    return mean;
+};
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import './style.css';
 import { paintBtn, removeBtnsPaint } from './domManipulation.js';
 import showHomeTab from './home.js';
 import showMenuTab from './menu.js'
-// import showContactTab from './contact.js';
+import showContactTab from './contact.js';
 import showAboutTab from './about';
 
 const homeBtn = document.querySelector('#home');
 const menuBtn = document.querySelector('#menu');
-// const contactBtn = document.querySelector('#contact');
+const contactBtn = document.querySelector('#contact');
 const aboutBtn = document.querySelector('#about');
 
 window.onload = paintBtn(homeBtn), showHomeTab();
@@ -24,11 +24,11 @@ menuBtn.addEventListener('click', () => {
     paintBtn(menuBtn);
 });
 
-// contactBtn.addEventListener('click', () => {
-//     removeBtnsPaint();
-//     paintBtn(contactBtn);
-//     // showContactTab();
-// });
+contactBtn.addEventListener('click', () => {
+    removeBtnsPaint();
+    paintBtn(contactBtn);
+    showContactTab();
+});
 
 aboutBtn.addEventListener('click', () => {
     removeBtnsPaint();
